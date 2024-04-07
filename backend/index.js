b114c4e49624899872d815d0fd44f4e0207f37a0
chynnaleigh <chynnaleigha@gmail.com>
2024-04-06 20:17:38 -0600

    reverted URL back

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.listen(PORT, () => {
 
 app.use(
     cors({
-        origin: ["https://sustainabee.onrender.com"],
+        origin: ["http://localhost:3000"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
