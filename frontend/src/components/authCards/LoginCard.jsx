0c511d82188436f7b87d98fe405788b2ae1e72a3
chynnaleigh <chynnaleigha@gmail.com>
2024-04-14 17:08:48 -0600

    Fix typo

--- frontend/src/components/authCards/LoginCard.jsx
+++ frontend/src/components/authCards/LoginCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie;";
+import { useCookies } from "react-cookie";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import {
